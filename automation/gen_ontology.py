1593766f295237dd7ed0f5b527b4daca35e6adc8
nicoSix <nicolasix@outlook.fr>
2021-09-29 13:25:16 +0200

    Adding generation of relations between patterns (paper or canonical)

--- automation/gen_ontology.py
+++ automation/gen_ontology.py
@@ -12,11 +12,18 @@ def capitalizeURI(word):
   else:
     return word
 
-def getPaperFromId(papers, paperId):
+def getPaperPatternFromId(papers, paper_id):
   for p in papers:
-    if p['ID'] == paperId:
+    if p['ID'] == paper_id:
       return p
+  return False
 
+def isNameInCanonicals(name, canonical_patterns):
+  for c in canonical_patterns:
+    if parseToURI(name) == parseToURI(c['Name']):
+      return True
+  return False
+  
 def getFirstAuthor(authors):
   return re.split(', ', authors)[0]
 
@@ -25,6 +32,33 @@ def parseToURI(name):
   nameArray = [capitalizeURI(word) for word in nameArray]
   name = ''.join(nameArray)
   return name
+
+def parseToRelation(name):
+  nameArray = re.split('[, \-!?:()/&*]+', name)
+  nameArray = [nameArray[0].lower()] + [capitalizeURI(nameArray[i]) for i in range(1, len(nameArray)) if i != 0]
+  name = ''.join(nameArray)
+  return name
+
+def get_links_between_patterns(paper_patterns, paper_pattern, paper, canonical_patterns):
+  link_types = ["From pattern", "Related to", "Variant Of", "Requires", "Benefits from"]
+  relation_template = loadTemplate('relation')
+  relations_str = ''
+
+  for key in link_types:
+    if key in paper_pattern:
+      relations = re.split(', ', paper_pattern[key])
+      for r in relations:
+        if r.isdigit():
+          relation_paper = getPaperPatternFromId(paper_patterns, r)
+          relation_paper_name = parseToURI(relation_paper['Name']) + getFirstAuthor(paper['author']) + paper['year']
+        else:
+          if isNameInCanonicals(r, canonical_patterns):
+            relation_paper_name = parseToURI(r) + "Canonical"
+          else:
+            relation_paper_name = "Unknown"
+        relations_str += Template(relation_template).substitute(relation=parseToRelation(key), value=relation_paper_name)
+
+  return relations_str
 
 def loadSLRData():
   with open('patterns_data.json', 'r') as file:
@@ -36,17 +70,15 @@ def loadTemplate(item):
     return file.read()
 
 def createExempleToCanonicalMappings(canonical_patterns):
-  canonical_mapping = {}
   example_mapping = {}
 
   for c in canonical_patterns:
     if 'Alternative names' in c:
-      canonical_mapping[parseToURI(c['Name'])] = [parseToURI(an) for an in c['Alternative names'].split(', ')]
       for an in c['Alternative names'].split(', '):
         example_mapping[parseToURI(an)] = parseToURI(c['Name'])
     example_mapping[parseToURI(c['Name'])] = parseToURI(c['Name'])
 
-  return canonical_mapping, example_mapping
+  return example_mapping
 
 def parseToOntologyLiteralIfExists(item, key):
   if key in item:
@@ -62,7 +94,7 @@ def run():
   class_template = loadTemplate('class')
   canonical_template = loadTemplate('canonical')
   example_template = loadTemplate('paper')
-  canonical_mapping, example_mapping = createExempleToCanonicalMappings(canonical_patterns)
+  example_mapping = createExempleToCanonicalMappings(canonical_patterns)
 
   for p in canonical_patterns:
     patternType = parseToURI(p['Type (determined)'])
@@ -94,7 +126,7 @@ def run():
     )
 
   for p in paper_patterns:
-    paper = getPaperFromId(papers, p['Paper'])
+    paper = getPaperPatternFromId(papers, p['Paper'])
     patternType = parseToURI(p['Type (determined)'])
     if patternType == "ArchitecturalPattern" or patternType == "Idiom":
       patternCategory = patternType
@@ -114,7 +146,8 @@ def run():
       context=parseToOntologyLiteralIfExists(p, 'Context & Problem'), 
       solution=parseToOntologyLiteralIfExists(p, 'Solution'),
       author=getFirstAuthor(paper['author']),
-      year=paper['year']
+      year=paper['year'],
+      links=get_links_between_patterns(paper_patterns, p, paper, canonical_patterns)
     )
   
   with open("./results/classes.ttl", "w") as text_file_classes:
