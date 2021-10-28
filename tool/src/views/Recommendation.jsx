dc0c87f32df544709e1a9f33b46826f639211a80
nicoSix <nicolas.six.fr@gmail.com>
2021-10-28 02:59:51 +0200

    Adding temporary placeholder

--- tool/src/views/Recommendation.jsx
+++ tool/src/views/Recommendation.jsx
@@ -1,7 +1,31 @@
 import * as React from 'react';
+import { Typography, Container, Paper } from '@mui/material';
+import ContentContainer from '../layouts/ContentContainer';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => ({
+  paper: {
+    padding: "20px",
+    marginTop: "20px"
+  }
+}));
 
 export default function Recommendation() {
+  const classes = useStyles();
+
   return (
-    <div>Hello recommendation</div>
+    <ContentContainer>
+      <Container>
+        <Paper className={classes.paper}>
+          <Typography variant="h2" component="div">
+            In construction 🚧
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            This part of our ontology explorer is still in construction.
+            Please come back later!
+          </Typography>
+        </Paper>
+      </Container>
+    </ContentContainer>
   );
 }
